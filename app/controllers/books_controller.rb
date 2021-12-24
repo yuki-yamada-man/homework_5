c4507395e1281cc2b647be3b7af9c189fc27c449
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-12-24 07:28:26 +0000

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end 
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
+  
+end
