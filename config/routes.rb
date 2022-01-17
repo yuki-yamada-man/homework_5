1665c387f2f849eab8a10107a0bd144b0d8d3c40
yuki-yamada-man <major.in.from@i.softbank.jp>
2022-01-17 12:50:52 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
-  get "home/about" => "home#about"
+  get "home/about" => "homes#about"
   resources :books
 end
